ac6da48e805cffdde81f2730af4bb0a9f993ead9
Pawel Kotoniak <kotoniak@gmail.com>
2019-08-22 16:33:34 +0200

    create popup, displaying plugin activities

---
+++ popup.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function() {
+    document.addEventListener('DOMContentLoaded', function() {
+        const status = document.getElementById('status');
+        status.textContent = "Extension loaded";
+
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {}, function(resp) {
+                status.textContent = "Loading...";
+                console.log("Success");
+            });
+        });
+    });
+})();
