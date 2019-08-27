1b8f6ab5f98ce61e29aa8e8cd001bd3e056a63ff
Pawel Kotoniak <kotoniak@gmail.com>
2019-08-27 12:50:11 +0200

    Extracting data from the DOM and displaying JSON

--- scripts/addButtons.js
+++ scripts/addButtons.js
@@ -1,5 +1,24 @@
-const showMessage = function () {
-    alert("Show data");
+const showMessage = function (e) {
+    const element = e.srcElement.offsetParent.offsetParent;
+    
+    const authorAndUrl = element.querySelector('.fwn .fcg');
+    const timestamp = element.querySelector('a._5pcq').children[0];
+
+    let id = element.id.slice(10);
+    id = id.slice(-id.length, -4);
+
+    const author = authorAndUrl.children[0].textContent || '';
+    const url = `${element.baseURI}/permalink/${id}`;
+    const content = element.querySelector('.userContent').innerHTML || '';
+    const time = timestamp.title || '';
+    const uTime = timestamp.dataset.utime || '';
+
+    const dataJSON = JSON.stringify({
+        author, url, content, time, uTime
+    });
+
+    alert(dataJSON);
+    console.log(dataJSON);
 }
 
 const addButtons = function () {
