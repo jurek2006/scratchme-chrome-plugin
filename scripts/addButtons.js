57a418c901a5b4b979c6d301e7593013a5895c70
Jurek <jurek2006@gmail.com>
2020-03-13 15:42:42 +0100

    adding button and scrape person data

--- scripts/addButtons.js
+++ scripts/addButtons.js
@@ -1,93 +1,89 @@
 'use strict';
 
-const showMessage = (element) => {
-    const timestamp = element.querySelector('._5ptz');
-    let time, uTime;
+const showMessage = element => {
+  //   const timestamp = element.querySelector('._5ptz');
+  //   let time, uTime;
 
-    if (timestamp) {
-        time = timestamp.title || '';
-        uTime = timestamp.dataset.utime || '';
-    } else {
-        time = '';
-        uTime = '';
-    }
+  //   if (timestamp) {
+  //     time = timestamp.title || '';
+  //     uTime = timestamp.dataset.utime || '';
+  //   } else {
+  //     time = '';
+  //     uTime = '';
+  //   }
 
-    const { origin, pathname } = document.location;
+  //   const { origin, pathname } = document.location;
 
-    let url = document.location.origin;
-    let postId = Number(element.offsetParent.id.slice(10, -4));
+  //   let url = document.location.origin;
+  const url = document.location.href;
+  const name = element
+    .querySelector('.pv-top-card--list li.t-24')
+    .textContent.trim();
 
-    if (!!postId) {
-        url = `${origin}${pathname}permalink/${postId}`;
-    }
+  const company = element
+    .querySelector('[data-control-name="position_see_more"] span')
+    .textContent.trim();
 
-    const author = element.querySelector('span.fcg a').textContent || '';
+  const position = element.querySelector('h2.t-18').textContent.trim();
 
-    const stripTags = (str) => {
-        if (!str) {
-            return "";
-        } else {
-            const reg = /<([^>]+>)/ig;
-            return str.replace(reg, '');
-        }
-    }
+  console.log({ url, name, company, position });
+  //   let postId = Number(element.offsetParent.id.slice(10, -4));
 
-    let content = element.querySelector('.userContent') || '';
-    content = stripTags(content.innerHTML);
+  //   if (!!postId) {
+  //     url = `${origin}${pathname}permalink/${postId}`;
+  //   }
 
-    // Save data to background.js. The data are save in background and read in popup.js
-    chrome.runtime.sendMessage({ isClickedButton: true, postId, author, url, content, time, uTime });
-}
+  //   const author = element.querySelector('span.fcg a').textContent || '';
+
+  //   const stripTags = str => {
+  //     if (!str) {
+  //       return '';
+  //     } else {
+  //       const reg = /<([^>]+>)/gi;
+  //       return str.replace(reg, '');
+  //     }
+  //   };
+
+  //   let content = element.querySelector('.userContent') || '';
+  //   content = stripTags(content.innerHTML);
+
+  // Save data to background.js. The data are save in background and read in popup.js
+  chrome.runtime.sendMessage({
+    isClickedButton: true,
+    name,
+    company,
+    position,
+    url
+  });
+};
 
 const addButtons = () => {
-    
-    // Does not add a button when pathname does not contain "groups"
-    const reg = /groups/;
-    if(!reg.test(window.location.pathname)) return;
+  // Does not add a button when pathname does not contain "groups"
+  //   const reg = /in/;
+  //   if (!reg.test(window.location.pathname)) return;
 
-    
-    
-    const posts = document.querySelectorAll('._1dwg._1w_m._q7o');
+  const box = document.querySelector('#ember65');
+  const topCard = document.querySelector('.pv-top-card');
 
-    [].forEach.call(posts, (post) => {
-        const uiPopoverParrent = post.querySelector('div._4r_y');
-
-        if (uiPopoverParrent) {
-
-            let saveButton = document.createElement("button");
-            saveButton.classList.add('scratch-me-btn');
-            saveButton.setAttribute('title', "ScratchMe")
-
-            const strongText = document.createElement("strong");
-            strongText.innerText = "Scratch";
-            strongText.classList.add('strong-text');
-
-            const lightText = document.createTextNode("Me");
-
-            saveButton.appendChild(strongText);
-            saveButton.appendChild(lightText);
-            uiPopoverParrent.appendChild(saveButton);
-
-            saveButton.addEventListener('click', () => showMessage(post));
-        };
-    });
-}
-
-const scrollWindow = () => {
-    addButtons();
-    let timer;
-
-    window.addEventListener('scroll', () => {
-        if (timer)
-            window.clearTimeout(timer);
-
-        timer = window.setTimeout(addButtons, 100);
-    });
-}
-
+  console.log(box);
+  if (box && topCard) {
+    const saveButton = document.createElement('button');
+    saveButton.classList.add('scratch-me-btn');
+    saveButton.setAttribute('title', 'ScratchMe');
+    const strongText = document.createElement('strong');
+    strongText.innerText = 'Scratch';
+    strongText.classList.add('strong-text');
+    const lightText = document.createTextNode('IN');
+    saveButton.appendChild(strongText);
+    saveButton.appendChild(lightText);
+    console.log('adding', saveButton);
+    box.appendChild(saveButton);
+    saveButton.addEventListener('click', () => showMessage(topCard));
+  }
+};
 
 if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', scrollWindow);
+  document.addEventListener('DOMContentLoaded', addButtons);
 } else {
-    scrollWindow();
+  addButtons();
 }
