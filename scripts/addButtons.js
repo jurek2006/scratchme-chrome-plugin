bd7ce33b69d35ac8adbc2322a54d30168756d677
Jurek <jurek2006@gmail.com>
2020-03-17 13:40:26 +0100

    feat: adding scratchIn button to sticky header

--- scripts/addButtons.js
+++ scripts/addButtons.js
@@ -57,48 +57,84 @@ const showMessage = element => {
   });
 };
 
-const addButtons = () => {
-  // Does not add a button when pathname does not contain "groups"
-  //   const reg = /in/;
-  //   if (!reg.test(window.location.pathname)) return;
-
-  const createScratchBtn = () => {
-    const scratchButton = document.createElement('button');
-    scratchButton.classList.add('scratch-me-btn');
-    scratchButton.setAttribute('title', 'ScratchIn');
-    const strongText = document.createElement('strong');
-    strongText.innerText = 'Scratch';
-    strongText.classList.add('strong-text');
-    const lightText = document.createTextNode('IN');
-    scratchButton.appendChild(strongText);
-    scratchButton.appendChild(lightText);
-    return scratchButton;
-  };
-
+const addTopCardButton = btn => {
   const topCard = document.querySelector('.pv-top-card');
+  // stop if there's no topCard - there is no person LinkedIn data and space to add button
+  if (!topCard) return;
 
-  const connectBtn =
-    topCard && topCard.querySelector('.pv-s-profile-actions--connect');
+  // stop if there's already button in topCard
+  const isButtonAlready = topCard.querySelector('.scratch-me-btn'); // TEMP get class from button ?
+  if (isButtonAlready) return;
+
+  // create button with handler opening popup & passing profile data (element topCard to extract data from)
+  btn.addEventListener('click', () => showMessage(topCard));
+
+  const connectBtn = topCard.querySelector('.pv-s-profile-actions--connect');
 
   if (!connectBtn) return;
 
   // for some profiles (e.g. Bill Gates) "connect" button is nested in dropdown (in an element <artdeco-dropdown>)
   const dropdown = connectBtn.closest('artdeco-dropdown');
 
-  // actionBtnsBox is an element containing buttons "connect", "send message", "more..." (sometimes follow)
+  // actionBtnsBox is an element containing buttons (in topCard element) "connect", "send message", "more..." (sometimes follow)
   const actionBtnsBox = dropdown
     ? dropdown.parentNode.parentNode.parentNode
     : connectBtn.parentNode.parentNode.parentNode;
 
   if (actionBtnsBox) {
-    const scratchBtn = createScratchBtn();
-    actionBtnsBox.insertBefore(scratchBtn, actionBtnsBox.firstElementChild);
-    scratchBtn.addEventListener('click', () => showMessage(topCard));
+    actionBtnsBox.insertBefore(btn, actionBtnsBox.firstElementChild);
   }
 };
 
+const addStickyHeaderButton = btn => {
+  // Does not add a button when pathname does not contain "groups"
+  //   const reg = /in/;
+  //   if (!reg.test(window.location.pathname)) return;
+
+  const topCard = document.querySelector('.pv-top-card');
+  const stickyHeaderBox = document.querySelector(
+    '.pv-profile-sticky-header__actions-container div'
+  );
+
+  // if there's no topCard - there is no person LinkedIn data
+  // if there's no stkickyHeader Box - page is not scrolled to show sticky header
+  if (!topCard || !stickyHeaderBox) return;
+
+  const isButtonAlready = stickyHeaderBox.querySelector('.scratch-me-btn');
+  if (!isButtonAlready) {
+    stickyHeaderBox.insertBefore(scratchBtn, stickyHeaderBox.firstElementChild);
+  }
+};
+
+const createScratchBtn = () => {
+  const scratchButton = document.createElement('button');
+  scratchButton.classList.add('scratch-me-btn');
+  scratchButton.setAttribute('title', 'ScratchIn');
+  const strongText = document.createElement('strong');
+  strongText.innerText = 'Scratch';
+  strongText.classList.add('strong-text');
+  const lightText = document.createTextNode('IN');
+  scratchButton.appendChild(strongText);
+  scratchButton.appendChild(lightText);
+  return scratchButton;
+};
+
+const scratchBtn = createScratchBtn();
+
+const scrollWindow = () => {
+  addTopCardButton(scratchBtn);
+  addStickyHeaderButton(scratchBtn);
+  let timer;
+
+  window.addEventListener('scroll', () => {
+    if (timer) window.clearTimeout(timer);
+
+    timer = window.setTimeout(() => addStickyHeaderButton(scratchBtn), 100);
+  });
+};
+
 if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', addButtons);
+  document.addEventListener('DOMContentLoaded', scrollWindow);
 } else {
-  addButtons();
+  scrollWindow();
 }
